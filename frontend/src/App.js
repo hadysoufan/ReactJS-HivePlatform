b212c491425d60f42e902b142024f591d6c81399
hadysoufan <hadisoufan74@gmail.com>
2023-05-06 17:10:39 +0300

    Adding  route to the Hive Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomeScreen from './Screens/HomeScreen/HomeScreen.component';
+import HomeScreen from './Screens/HomeScreen/Home.Screen';
+import Hive from './Screens/HiveScreen/Hive.Screen';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} exact />
+        <Route path="/hive" element={<Hive />}/>
       </Routes>
     </Router>
   );
