15f236abce174f3727aaf5fb072759f6425367d3
hadysoufan <hadisoufan74@gmail.com>
2023-05-07 13:11:20 +0300

    Adding all colors to one file

---
+++ frontend/src/components/styled/theme.styled.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    backGround: '#0f0e17',
+    headline: '#fffffe',
+    paragraph: '#a7a9be',
+    button: '#ff8906',
+    secColor: '#ffedd3',
+    shadow1: 'rgba(255, 137, 6, 1)',
+    shadow2: 'rgba(255, 255, 255, 0.97)',
+    shadow3: 'rgba(0, 0, 0, 0.03)',
+    span: '#e67b05',
+    active: '#b36004',
+    light: '#ffffff',
+    dark: '#000',
+  },
+};
+
+export default theme;
