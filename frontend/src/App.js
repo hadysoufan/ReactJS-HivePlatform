93d2b804930f4af4393873c3c1ab1bdc139da67f
hadysoufan <hadisoufan74@gmail.com>
2023-05-07 13:13:21 +0300

    Importing and using ThemeProvider from styled component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './Screens/HomeScreen/Home.Screen';
 import Hive from './Screens/HiveScreen/Hive.Screen';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/components/styled/theme.styled';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} exact />
-        <Route path="/hive" element={<Hive />}/>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} exact />
+          <Route path="/hive" element={<Hive />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
