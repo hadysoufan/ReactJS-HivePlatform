3d45b1f705f5d7d429e100cf7a3053f401aea81a
hadysoufan <hadisoufan74@gmail.com>
2023-05-11 21:01:55 +0300

    Editing

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .users import users
 
-
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
