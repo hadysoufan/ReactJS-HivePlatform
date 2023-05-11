95ad3a582877484ddcf4389abb3cdfa858349904
hadysoufan <hadisoufan74@gmail.com>
2023-05-11 19:04:19 +0300

    Adding spacing object

--- frontend/src/components/styled/theme.styled.js
+++ frontend/src/components/styled/theme.styled.js
@@ -1,17 +1,37 @@
-const theme = {
+const theme = { 
   colors: {
+    // Home Colors
     backGround: '#0f0e17',
     headline: '#fffffe',
     paragraph: '#a7a9be',
     button: '#ff8906',
     secColor: '#ffedd3',
-    shadow1: 'rgba(255, 137, 6, 1)',
-    shadow2: 'rgba(255, 255, 255, 0.97)',
-    shadow3: 'rgba(0, 0, 0, 0.03)',
     span: '#e67b05',
     active: '#b36004',
-    light: '#ffffff',
-    dark: '#000',
+
+    // Hive Colors
+    dark: 'hsl(252, 30%, 17%)',
+    light: 'hsl(252, 30%, 95%)',
+    gray: 'hsl(252, 15%, 65%)',
+    primary: 'hsl(31.5, 95.7%, 46.1%)',
+    secondary: 'hsl(252, 100%, 90%)',
+    success: 'hsl(120, 95%, 65%)',
+    danger: 'hsl(0, 95%, 65%)',
+    black: 'hsl(252, 30%, 10%)',
+    white: 'hsl(252, 30%, 100%)',
+  },
+
+  spacing: {
+
+    // Hive Spacing
+    borderRadius: '2rem',
+    cardBorderRadius: '1rem',
+    btnPadding: '0.6rem 2rem',
+    searchPadding: '0.6rem 1rem',
+    cardPadding: '1rem',
+
+    stickyTopLeft: '5.4rem',
+    stickyTopRight: '-18rem',
   },
 };
 
