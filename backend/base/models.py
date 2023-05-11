a621876685d1fdfc545e8433f7f0a3509061db7c
hadysoufan <hadisoufan74@gmail.com>
2023-05-11 21:17:03 +0300

    Creating Post model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='post-image/', blank=True, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-update', '-created']
+
+    def __str__(self):
+        return str(self.host)
