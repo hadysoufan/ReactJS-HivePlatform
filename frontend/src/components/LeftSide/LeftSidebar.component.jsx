fa7d83e7ca450706d39dcfd54146c898482db364
hadysoufan <hadisoufan74@gmail.com>
2023-05-12 20:34:34 +0300

    Editing Link path

--- frontend/src/components/LeftSide/LeftSidebar.component.jsx
+++ frontend/src/components/LeftSide/LeftSidebar.component.jsx
@@ -1,54 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Profile1 from '../../assets/img/profile-1.jpg';
 import * as Left from './LeftSide.styled';
 import { Link } from 'react-router-dom';
 
 function LeftSidebar() {
-  const [menuItems, setMenuItems] = useState([]);
-
   useEffect(() => {
-    const items = document.querySelectorAll('.menu-item');
-    setMenuItems(items);
-    items.forEach((item) => {
-      item.addEventListener('click', () => {
-        changeActiveItem();
-        item.classList.add('active');
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach((menuItem) => {
+      menuItem.addEventListener('click', (event) => {
+        menuItems.forEach((menuItem) => {
+          menuItem.classList.remove('active');
+        });
+
+        event.currentTarget.classList.add('active');
       });
     });
-
-    return () => {
-      items.forEach((item) => {
-        item.removeEventListener('click', () => {
-          changeActiveItem();
-          item.classList.add('active');
-        });
-      });
-    };
   }, []);
 
-  const changeActiveItem = () => {
-    menuItems.forEach((item) => {
-      item.classList.remove('active');
-    });
-  };
   return (
     <div>
       <Left.LeftSide>
         {/* Profile section */}
-        <Link to="/profile" className="profile">
+        <Link to="/hive/user-profile" className="profile">
           <div className="profile-picture">
             <img src={Profile1} alt="" />
           </div>
           <div className="handle">
-            <h4>Diana Ayi</h4>
-            <p className="text-muted">@dayi</p>
+            <h4>Abir Zaiter</h4>
+            <p className="text-muted">@cleopatra</p>
           </div>
         </Link>
 
         {/* Side bar  */}
         <Left.SideBar>
           {/* Home link */}
-          <Link to='/' className="menu-item active">
+          <Link to="/hive" className="menu-item active">
             <span>
               <i className="uil uil-home"></i>
             </span>
@@ -60,7 +47,7 @@ function LeftSidebar() {
             <span>
               <i className="uil uil-compass"></i>
             </span>
-            <h3>Explore</h3>
+            <h3>Create Room</h3>
           </Link>
 
           {/* Message link */}
