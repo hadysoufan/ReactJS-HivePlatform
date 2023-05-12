11f3efa2a7968e658ed225da474023885f970bb2
hadysoufan <hadisoufan74@gmail.com>
2023-05-12 22:59:54 +0300

    Editing routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import HomeScreen from './Screens/HomeScreen/Home.Screen';
 import Hive from './Screens/HiveScreen/Hive.Screen';
-import { ThemeProvider } from 'styled-components';
-import theme from './components/styled/theme.styled';
 import CreatePost from './Screens/CreatePost/CreatePost.Screen';
 import PostScreen from './Screens/Post/Post.Screen';
 import ProfileScreen from './Screens/Profile/Profile.Screen';
+
+import theme from './components/styled/theme.styled';
 
 function App() {
   return (
