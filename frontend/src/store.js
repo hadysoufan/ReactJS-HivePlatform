0a25b7aed3d7c11a93748ef8b23355016205384b
hadysoufan <hadisoufan74@gmail.com>
2023-05-12 22:57:39 +0300

    Using react reducers

---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { feedListReducer } from './reducers/feedReducer';
+
+const reducer = combineReducers({
+  feedList: feedListReducer,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
