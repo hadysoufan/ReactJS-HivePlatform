84f857b3f2fa7409db4858d39843af069d7a0383
hadysoufan <hadisoufan74@gmail.com>
2023-05-12 23:01:26 +0300

    Editing urls

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name='routes'),
     path('feeds/', views.getFeeds, name='feeds'),
     path('feeds/<str:pk>', views.getFeed, name='feed'),
 ]
