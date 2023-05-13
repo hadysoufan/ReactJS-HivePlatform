080fed2fe673220adee3795c1a87130c20a80099
hadysoufan <hadisoufan74@gmail.com>
2023-05-13 09:11:00 +0300

    JWT routes

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    path('', views.getRoutes, name='routes'),
     path('feeds/', views.getFeeds, name='feeds'),
     path('feeds/<str:pk>', views.getFeed, name='feed'),
 ]
