443a45ee1d5d97b9a7741de761e9b31bf588b624
hadysoufan <hadisoufan74@gmail.com>
2023-05-13 11:14:51 +0300

    Editing routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hive/1.0.0/', include('base.urls')),
+    #path('hive/1.0.0/', include('base.urls')),
+
+    path('hive/1.0.0/users/', include('base.urls.users_urls')),
+    path('hive/1.0.0/feeds/', include('base.urls.feeds_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
