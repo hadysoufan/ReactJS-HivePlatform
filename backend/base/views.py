9f999a454264f498c272c05bfabe92fbf990ac34
hadysoufan <hadisoufan74@gmail.com>
2023-05-13 09:10:48 +0300

    JWT Customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,30 @@ from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data    
+        
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer    
+
+@api_view(['GET'])
+def getRoutes(reqeust):
+    routes = [
+        'hive/1.0.0/feeds',
+        'hive/1.0.0/feeds/<str:pk>',
+    ]
+    return Response(routes)
 
 @api_view(['GET'])
 def getFeeds(request):
